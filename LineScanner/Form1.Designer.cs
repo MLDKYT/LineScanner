7cf1edac04eb3671b83db70e79bbf0bc28102204
mldkyt <mldkyt@proton.me>
2022-12-27 23:34:01 +0100

    First release

---
+++ LineScanner/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LineScanner {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Line Scanner";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Scan the amount of lines of code in a folder!\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select drive";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(312, 105);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "The results will appear here...";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(312, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Scan!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 223);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Line Scanner";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ LineScanner/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LineScanner {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private string pathToScan;
+        private int scannedDirectories, scannedFiles, scannedLinesOfCode;
+
+        private string[] fileFormats = new[] {
+            "cs", "js", "css", "html", "htm", "php", "sql", "xml", "json", "txt", "md",
+            "bat", "ini", "config", "csproj", "sln", "resx", "xaml", "vb", "vbproj", "aspx", "ascx", "cshtml",
+            "config", "csproj", "sln", "vb", "vbproj", "resx", "xaml"
+        };
+
+        private void button1_Click(object sender, EventArgs e) {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK) {
+                pathToScan = fbd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (string.IsNullOrEmpty(pathToScan)) {
+                MessageBox.Show("Please select a folder to scan", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _ = StartScanDirectory(pathToScan);
+        }
+
+        async Task StartScanDirectory(string path) {
+            await ScanDirectory(path);
+            UpdateLabel(path);
+        }
+        
+        string SplitNumber(int num) {
+            // split number using ' every 3 digits
+            var str = num.ToString();
+            var result = "";
+            var count = 0;
+            for (var i = str.Length - 1; i >= 0; i--) {
+                result = str[i] + result;
+                count++;
+                if (count == 3 && i != 0) {
+                    result = "'" + result;
+                    count = 0;
+                }
+            }
+            
+            return result;
+        }
+
+        void UpdateLabel(string path) {
+            label3.Text = $@"Scanned folders: {SplitNumber(scannedDirectories)}
+Scanned files: {SplitNumber(scannedFiles)}
+Lines of code found: {SplitNumber(scannedLinesOfCode)}
+Currently scanning: {path.ToLower()}";
+        }
+
+        private int temp1 = 0;
+        
+        async Task ScanDirectory(string path) {
+            var files = Directory.GetFiles(path);
+            foreach (var file in files) {
+                try {
+                    if (fileFormats.Contains(Path.GetExtension(file).Substring(1))) {
+                        scannedFiles += 1;
+                        var lines = File.ReadAllLines(file);
+                        scannedLinesOfCode += lines.Length;
+                    }
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            var directories = Directory.GetDirectories(path);
+            foreach (var directory in directories) {
+                scannedDirectories += 1;
+                await ScanDirectory(directory);
+            }
+
+            temp1++;
+            if (temp1 > 10) {
+                temp1 = 0;
+                UpdateLabel(path);
+                await Task.Delay(1);
+            }
+            UpdateLabel(path);
+        }
+    }
+}
